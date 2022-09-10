00a1e6e19489120e71c2d627fecafae4510f75a4
woowang789 <wangjh789@gmail.com>
2022-09-11 02:42:55 +0900

    first commit

---
+++ src/test/java/com/example/gitactiontest/GitActionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitactiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
