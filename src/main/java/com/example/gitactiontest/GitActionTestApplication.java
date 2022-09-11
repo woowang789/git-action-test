90ed55dd6cf977a8440daa291c8c4e78f68dc8c4
woowang789 <wangjh789@gmail.com>
2022-09-11 13:35:48 +0900

    deploy test

--- src/main/java/com/example/gitactiontest/GitActionTestApplication.java
+++ src/main/java/com/example/gitactiontest/GitActionTestApplication.java
@@ -12,7 +12,7 @@ public class GitActionTestApplication {
 
     @GetMapping
     public String test(){
-        return "okkkk";
+        return "okkkkkkk";
     }
 
     public static void main(String[] args) {
